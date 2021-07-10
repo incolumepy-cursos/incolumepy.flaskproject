e7e35dab2b00f7c2b6d5478574a105bdfd8dc2d7
Britodfbr <britodfbr@gmail.com>
2021-07-10 08:57:54 -0300

    SECRET_KEY acrescentada a configuração do Flask
    o pacote secrets possui python-ldap como dependência, o qual não
    funciona com python 3.9.6, e gerei a key com strings + random

--- incolumepy/flaskproject/main.py
+++ incolumepy/flaskproject/main.py
@@ -1,43 +1,44 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "FVrpmovvW7LblKlVFmfLIk4n8X7sjuUOdIwe97udBDwLrVQtWge1cpKTwm0u137JovWZKDtFQXvZtOfBE0dLAz8H7LV"
 
 posts = [
     {
-        'id': 5,
-        'author': 'Ada Catarina Santos Brito',
-        'title': 'post 5',
-        'content': 'content post 5',
-        'posted': '09-07-2021 15:58',
+        "id": 5,
+        "author": "Ada Catarina Santos Brito",
+        "title": "post 5",
+        "content": "content post 5",
+        "posted": "09-07-2021 15:58",
     },
     {
-        'id': 4,
-        'author': 'Ana Gabriela Santos Brito',
-        'title': 'post 4',
-        'content': 'content post 4',
-        'posted': '09-07-2021 15:58',
+        "id": 4,
+        "author": "Ana Gabriela Santos Brito",
+        "title": "post 4",
+        "content": "content post 4",
+        "posted": "09-07-2021 15:58",
     },
     {
-        'id': 3,
-        'author': 'Eliana Ferreira dos Santos Brito',
-        'title': 'post 3',
-        'content': 'content post 3',
-        'posted': '09-07-2021 15:53',
+        "id": 3,
+        "author": "Eliana Ferreira dos Santos Brito",
+        "title": "post 3",
+        "content": "content post 3",
+        "posted": "09-07-2021 15:53",
     },
     {
-        'id': 2,
-        'author': 'Ricardo Brito do Nascimento',
-        'title': 'post 2',
-        'content': 'content post 2',
-        'posted': '09-07-2021 15:03',
+        "id": 2,
+        "author": "Ricardo Brito do Nascimento",
+        "title": "post 2",
+        "content": "content post 2",
+        "posted": "09-07-2021 15:03",
     },
     {
-        'id': 1,
-        'author': 'Ricardo Brito do Nascimento',
-        'title': 'post 1',
-        'content': 'content post 1',
-        'posted': '09-07-2021 15:00',
-    }
+        "id": 1,
+        "author": "Ricardo Brito do Nascimento",
+        "title": "post 1",
+        "content": "content post 1",
+        "posted": "09-07-2021 15:00",
+    },
 ]
 
 
@@ -46,7 +47,7 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route('/')
+@app.route("/")
 def home():
     title = "Home Page"
-    return render_template('home.html', title=title, posts=posts)
+    return render_template("home.html", title=title, posts=posts)
