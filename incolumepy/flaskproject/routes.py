5e90876ffabad8d8fcaced7c5761acf020463c70
Britodfbr <britodfbr@gmail.com>
2021-07-12 16:07:16 -0300

    Configuração para Atualização da informação do usuário

--- incolumepy/flaskproject/routes.py
+++ incolumepy/flaskproject/routes.py
@@ -6,7 +6,7 @@ from random import choices
 from string import hexdigits
 from flask import render_template, flash, url_for, redirect, request
 from . import app, db, bc
-from .forms import RegistrationForm, LoginForm
+from .forms import RegistrationForm, LoginForm, UpdateAccountForm
 from .models import posts, User
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -80,4 +80,17 @@ def save_picture(form_pic):
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Conta atualizada com sucesso", "success")
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename="profile_pics/{}".format(current_user.image_file))
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
