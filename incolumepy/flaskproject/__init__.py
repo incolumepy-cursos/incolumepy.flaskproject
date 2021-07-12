0fdf2a03a33a5c09d13a5e1381570f4ef33a339c
Britodfbr <britodfbr@gmail.com>
2021-07-11 23:59:19 -0300

    Definido login_manager.login_view para páginas que requerem login obrigatório

--- incolumepy/flaskproject/__init__.py
+++ incolumepy/flaskproject/__init__.py
@@ -13,5 +13,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask_blog.db"
 db = SQLAlchemy(app)
 bc = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from . import routes
