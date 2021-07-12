5a6e319cf8bfb60f62c55c3303f4f914a6e426a5
Britodfbr <britodfbr@gmail.com>
2021-07-11 23:13:57 -0300

    Models preparados para login

--- incolumepy/flaskproject/__init__.py
+++ incolumepy/flaskproject/__init__.py
@@ -4,6 +4,7 @@ __author__ = '@britodfbr'
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "FVrpmovvW7LblKlVFmfLIk4n8X7sjuUOdIwe97udBDwLrVQtWge1cpKTwm0u137JovWZKDtFQXvZtOfBE0dLAz8H7LV"
@@ -11,5 +12,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask_blog.db"
 
 db = SQLAlchemy(app)
 bc = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from . import routes
--- incolumepy/flaskproject/models.py
+++ incolumepy/flaskproject/models.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = '@britodfbr'
-from . import db
 import datetime as dt
+from . import db, login_manager
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
