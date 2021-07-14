c68db1b53bb3590d59f2b71f923e42b3999e9e35
Britodfbr <britodfbr@gmail.com>
2021-07-14 08:10:49 -0300

    correção na chamada do atributo current_user.is_authenticated

--- incolumepy/flaskproject/routes.py
+++ incolumepy/flaskproject/routes.py
@@ -176,7 +176,7 @@ def send_reset_email(user):
 
 @app.route('reset_password', methods=['GET', 'POST'])
 def reset_request():
-    if current_user.is_authenticate():
+    if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = RequestResetForm()
     if form.validate_on_submit():
@@ -189,7 +189,7 @@ def reset_request():
 
 @app.route("/reset_password/<token>", methods=["GET", "POST"])
 def reset_token(token):
-    if current_user.is_authenticate():
+    if current_user.is_authenticated:
         return redirect(url_for('home'))
     user = User.verify_reset_token(token)
     if user is None:
