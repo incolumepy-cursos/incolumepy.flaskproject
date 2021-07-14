e00104d016a78e0fd0daf1d8cbd4e3c7243220d5
Britodfbr <britodfbr@gmail.com>
2021-07-14 07:26:50 -0300

    Realocação dos metodos de token

--- incolumepy/flaskproject/models.py
+++ incolumepy/flaskproject/models.py
@@ -20,17 +20,6 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    posted = db.Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
     def get_reset_token(self, expire_sec=1800):
         s = Serializer(app.config['SECRET_KEY'], expire_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
@@ -43,6 +32,17 @@ class Post(db.Model):
         except:
             return None
         return User.query.get(user_id)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    posted = db.Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.posted}')"
