0d7f34743983093abe7b268c1b7cff17d28855fd
Britodfbr <britodfbr@gmail.com>
2021-07-14 23:50:39 -0300

    Separação e proteção da configuração

--- incolumepy/flaskproject/__init__.py
+++ incolumepy/flaskproject/__init__.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = '@britodfbr'
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from incolumepy.flaskproject.configure import Config
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "FVrpmovvW7LblKlVFmfLIk4n8X7sjuUOdIwe97udBDwLrVQtWge1cpKTwm0u137JovWZKDtFQXvZtOfBE0dLAz8H7LV"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask_blog.db"
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 bc = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config["MAIL_SERVER"] = "smtp.googlemail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.environ.get('EMAIL_USER')
-app.config["MAIL_PASSWORD"] = os.environ.get('EMAIL_PW')
+
 mail = Mail(app)
 
 from incolumepy.flaskproject.main.routes import main
