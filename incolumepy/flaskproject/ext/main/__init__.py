f0afed316debbfae46e36a378e84536435ffeee8
Britodfbr <britodfbr@gmail.com>
2021-07-17 00:38:26 -0300

    Blueprint main aplicado por factory pattern

--- incolumepy/flaskproject/ext/main/__init__.py
+++ incolumepy/flaskproject/ext/main/__init__.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = '@britodfbr'
+
+from .routes import bp
+
+
+def init_app(app):
+    app.register_blueprint(bp)
--- incolumepy/flaskproject/ext/main/routes.py
+++ incolumepy/flaskproject/ext/main/routes.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = '@britodfbr'
+
 from flask import Blueprint, request, render_template, abort
-from incolumepy.flaskproject.models import Post
+# from incolumepy.flaskproject.models import Post
 from markdown import markdown
 from pathlib import Path
+bp = Blueprint('main', __name__)
 
-main = Blueprint('main', __name__)
 
-
-@main.route("/")
-@main.route("/home")
+@bp.route("/")
+@bp.route("/home")
 def home():
     title = "Home Page"
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.id.desc()).paginate(page=page, per_page=3)
-    return render_template("home.html", title=title, posts=posts)
+    return title
+    # posts = Post.query.order_by(Post.id.desc()).paginate(page=page, per_page=3)
+    # return render_template("home.html", title=title, posts=posts)
 
 
-@main.route("/about")
+@bp.route("/about")
 def about():
     f = Path(__file__[:__file__.index('incolumepy')]).joinpath('README.md')
     content = f.read_text()
@@ -26,31 +27,31 @@ def about():
     return render_template("generic_page.html", content=html, title='About')
 
 
-@main.route("/hello")
+@bp.route("/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@main.route('/401')
+@bp.route('/401')
 def error401():
     abort(401)
 
 
-@main.route('/403')
+@bp.route('/403')
 def error403():
     abort(403)
 
 
-@main.route('/404')
+@bp.route('/404')
 def error404():
     abort(404)
 
 
-@main.route('/405')
+@bp.route('/405')
 def error405():
     abort(405)
 
 
-@main.route('/500')
+@bp.route('/500')
 def error500():
     abort(500, "Ops.. Erro interno")
