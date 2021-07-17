1c0006071ba0c6aa046c80642f79d1b4e843ca21
Britodfbr <britodfbr@gmail.com>
2021-07-17 11:10:33 -0300

    Reestruturação para blueprint com factory partner

--- incolumepy/flaskproject/__init__.py
+++ incolumepy/flaskproject/__init__.py
@@ -2,36 +2,49 @@
 # -*- coding: utf-8 -*-
 __author__ = '@britodfbr'
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-from flask_mail import Mail
-from incolumepy.flaskproject.configure import Config
+from incolumepy.flaskproject import configure
+from incolumepy.flaskproject.ext import (
+    main,
+    auth,
+    cli,
+    dbase,
+    errors,
+    posts,
+    users,
+    mail,
+)
+#
+#
+#
+#
+# def create_app(config_class=Config, *args, **kwargs):
+#     app = Flask(__name__)
+#     app.config.from_object(config_class)
+#     db.init_app(app)
+#     bc.init_app(app)
+#     mail.init_app(app)
+#
+#     from incolumepy.flaskproject.ext.main.routes import main
+#     from incolumepy.flaskproject.ext.users.routes import users
+#     from incolumepy.flaskproject.ext.bp.routes import bp
+#     from incolumepy.flaskproject.ext.bp.handlers import bp
+#     app.register_blueprint(main)
+#     app.register_blueprint(users)
+#     app.register_blueprint(bp)
+#     app.register_blueprint(bp)
+#
+#     return app
 
 
-db = SQLAlchemy()
-bc = Bcrypt()
-login_manager = LoginManager()
-login_manager.login_view = 'users.login'
-login_manager.login_message_category = 'info'
-mail = Mail()
-
-
-def create_app(config_class=Config, *args, **kwargs):
+def create_app(*args, **kwargs):
     app = Flask(__name__)
-    app.config.from_object(config_class)
-    db.init_app(app)
-    bc.init_app(app)
-    login_manager.init_app(app)
+    configure.init_app(app)
+    errors.init_app(app)
+    cli.init_app(app)
+    dbase.init_app(app)
     mail.init_app(app)
-
-    from incolumepy.flaskproject.ext.main.routes import main
-    from incolumepy.flaskproject.ext.users.routes import users
-    from incolumepy.flaskproject.ext.posts.routes import posts
-    from incolumepy.flaskproject.ext.errors.handlers import errors
-    app.register_blueprint(main)
-    app.register_blueprint(users)
-    app.register_blueprint(posts)
-    app.register_blueprint(errors)
-
+    auth.init_app(app)
+    main.init_app(app)
+    users.init_app(app)
+    posts.init_app(app)
     return app
