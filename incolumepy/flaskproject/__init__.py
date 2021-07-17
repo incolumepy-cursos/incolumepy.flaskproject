9e0bd598aa8713b80ca1c3a5fb99428760892d92
Britodfbr <britodfbr@gmail.com>
2021-07-17 11:12:22 -0300

    Limpeza de estrutura obsoleta

--- incolumepy/flaskproject/__init__.py
+++ incolumepy/flaskproject/__init__.py
@@ -13,27 +13,6 @@ from incolumepy.flaskproject.ext import (
     users,
     mail,
 )
-#
-#
-#
-#
-# def create_app(config_class=Config, *args, **kwargs):
-#     app = Flask(__name__)
-#     app.config.from_object(config_class)
-#     db.init_app(app)
-#     bc.init_app(app)
-#     mail.init_app(app)
-#
-#     from incolumepy.flaskproject.ext.main.routes import main
-#     from incolumepy.flaskproject.ext.users.routes import users
-#     from incolumepy.flaskproject.ext.bp.routes import bp
-#     from incolumepy.flaskproject.ext.bp.handlers import bp
-#     app.register_blueprint(main)
-#     app.register_blueprint(users)
-#     app.register_blueprint(bp)
-#     app.register_blueprint(bp)
-#
-#     return app
 
 
 def create_app(*args, **kwargs):
