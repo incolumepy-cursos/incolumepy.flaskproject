def82625a1f9d05ff776896b082d19a0747809d8
Britodfbr <britodfbr@gmail.com>
2021-07-17 00:45:39 -0300

    Blueprint error aplicado por factory pattern

--- incolumepy/flaskproject/ext/errors/handlers.py
+++ incolumepy/flaskproject/ext/errors/handlers.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 __author__ = '@britodfbr'
 from flask import Blueprint, render_template
-errors = Blueprint('errors', __name__)
+bp = Blueprint('error', __name__)
 
 
-@errors.app_errorhandler(401)
-@errors.app_errorhandler(403)
-@errors.app_errorhandler(404)
-@errors.app_errorhandler(405)
-@errors.app_errorhandler(500)
+@bp.app_errorhandler(401)
+@bp.app_errorhandler(403)
+@bp.app_errorhandler(404)
+@bp.app_errorhandler(405)
+@bp.app_errorhandler(500)
 def error_page(e):
     # note that we set the HTTP status code explicitly
     return render_template('error.html', error=e, title=e.name), e.code
